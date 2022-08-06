f4e14d3597399dcc145d94a1c8c5b41b85e0d7ef
unknown <you@example.com>
2022-08-06 20:06:05 +0600

    added all task

---
+++ script.js
@@ -0,0 +1,32 @@
+function addMore(){
+    let GivenName = document.getElementById("tboxName").value
+    let box = document.getElementById("box")
+
+    let li = document.createElement("li")
+    li.innerHTML = GivenName
+
+    let a = document.createElement("a")
+    a.innerHTML= "X"
+    a.style.textDecoration = "none"
+    li.appendChild(a)
+
+    let pos = box.firstElementChild
+    if(pos==null){
+        box.appendChild(li)
+    }
+    else{
+        box.insertBefore(li,pos)
+    }
+    document.getElementById("tboxName").value = ""
+    document.getElementById("tboxName").focus 
+    document.getElementById("count").innerHTML = box.childElementCount
+}
+
+let btn = document.querySelector("ul")
+btn.addEventListener("click",function(e){
+    
+    let box = document.getElementById("box")
+    let li = e.target.parentNode
+    box.removeChild(li)
+    document.getElementById("count").innerHTML = box.childElementCount
+})
